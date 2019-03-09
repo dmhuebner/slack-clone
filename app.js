cacc0db09c97ab5b345c5d2af35be52ed6ba6926
David <davidmh.mail@gmail.com>
2019-03-09 00:13:23 -0600

    Emit rooms from namespace and add them to the DOM

--- app.js
+++ app.js
@@ -1,43 +1,33 @@
 const express = require('express');
 const app = express();
 const socketio = require('socket.io');
-
 const namespaces = require('./data/namespaces');
-
-// Loop through each namespace and listen for a connection
-namespaces.foreach(namespace => {
-    console.log(namespace);
-});
 
 app.use(express.static(__dirname + '/public'));
 
 const expressServer = app.listen(9000);
 const io = socketio(expressServer);
+
 // io.on = io.of('/').on
-io.on('connection',(socket)=>{
-    console.log("Someone connected to the main namespace")
-    socket.emit('messageFromServer',{data:"Welcome to the socketio server"});
-    socket.on('messageToServer',(dataFromClient)=>{
-        console.log(dataFromClient)
+io.on('connection', (socket) => {
+    // Build an array with an image and endpoint for each namespace
+    const nsData = namespaces.map(ns => {
+        return {
+            img: ns.img,
+            endpoint: ns.endpoint
+        };
     });
-    socket.on('newMessageToServer',(msg)=>{
-        // console.log(msg)
-        // io.emit('messageToClients',{text:msg.text})
-        io.of('/').emit('messageToClients',{text:msg.text})
-    });
-    // The server can still communicate across namespaces
-    // but on the clientInformation, the socket needs be in THAT namespace
-    // in order to get the events
 
-    setTimeout(()=>{
-        io.of('/admin').emit('welcome',"Welcome to the admin channel, from the main channel!")
-    },2000)
-
-
-
+    // Send nsData back to the client we need to use socket not io because it should go to just this client
+    socket.emit('nsList', nsData);
 });
 
-io.of('/admin').on('connection',(socket)=>{
-    console.log("Someone connected to the admin namespace!")
-    io.of('/admin').emit('welcome',"Welcome to the admin channel!");
+// Loop through each namespace and listen for a connection
+namespaces.forEach(namespace => {
+    io.of(namespace.endpoint).on('connection', (nsSocket) => {
+        console.log(`${nsSocket.id} has joined ${namespace.endpoint}`);
+        // A socket has connected to one of the chat group namespaces
+        // Send that namespace group's info back
+        nsSocket.emit('nsRoomLoad', namespace.rooms);
+    });
 });
--- public/scripts.js
+++ public/scripts.js
@@ -1,31 +1,61 @@
-const username = prompt("What is your username?");
-// const socket = io('http://localhost:9000'); // the / namespace/endpoint
-const socket = io('http://localhost:9000',{
-    query: {
-        username
-    }
+const socket = io('http://localhost:9000');
+
+console.log(socket.io);
+
+socket.on('connect', () => {
+    console.log(socket.id)
 });
-let nsSocket = "";
-// listen for nsList, which is a list of all the namespaces.
-socket.on('nsList',(nsData)=>{
-    console.log("The list of .rooms has arrived!!");
-    // console.log(nsData)
+
+// Listen for nsList which is a list of all the namespaces
+socket.on('nsList', (nsData) => {
+    console.log('The list of namespaces has arrived', nsData);
     let namespacesDiv = document.querySelector('.namespaces');
-    namespacesDiv.innerHTML = "";
-    nsData.forEach((ns)=>{
-        namespacesDiv.innerHTML += `<div class="namespace" ns=${ns.endpoint} ><img src="${ns.img}" /></div>`
+    namespacesDiv.innerHTML = '';
+    nsData.forEach(namespace => {
+        namespacesDiv.innerHTML += `<div class="namespace" ns="${namespace.endpoint}"><img src="${namespace.img}" alt="namespace-image" /></div>`;
     });
 
-    // Add a clicklistener for each NS
-    console.log(document.getElementsByClassName('namespace'))
-    Array.from(document.getElementsByClassName('namespace')).forEach((elem)=>{
-        // console.log(elem)
-        elem.addEventListener('click',(e)=>{
-            const nsEndpoint = elem.getAttribute('ns');
-            // console.log(`${nsEndpoint} I should go to now`)
-            joinNs(nsEndpoint)
-        })
+    // Add a click listener for each namespace
+    Array.from(document.getElementsByClassName('namespace')).forEach(element => {
+        element.addEventListener('click', (event) => {
+            const nsEndpoint = element.getAttribute('ns');
+            console.log('nsEndpoint', nsEndpoint);
+        });
     });
-    joinNs('/wiki');
+
+    const nsSocket = io('http://localhost:9000/wiki');
+    console.log('http://localhost:9000/wiki');
+    nsSocket.on('nsRoomLoad', (nsRooms) => {
+        console.log('nsRooms', nsRooms);
+        let roomList = document.querySelector('.room-list');
+        roomList.innerHTML = '';
+        nsRooms.forEach(room => {
+            const glyph = room.privateRoom ? 'glyphicon-lock' : 'glyphicon-globe';
+            roomList.innerHTML += `<li class="room"><span class="glyphicon ${glyph}"></span>${room.roomTitle}</li>`;
+        });
+
+        // Add a listener to each room
+        let roomNodes = document.getElementsByClassName('room');
+        Array.from(roomNodes).forEach(elem => {
+            elem.addEventListener('click', (event) => {
+                console.log(`someone clicked on ${event.target.innerText}`);
+            });
+        });
+    });
 });
 
+socket.on('messageFromServer', (dataFromServer) => {
+    console.log(dataFromServer);
+    socket.emit('dataToServer', {data: "Data from the Client!"})
+});
+
+document.querySelector('#message-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const newMessage = document.querySelector('#user-message').value;
+    socket.emit('newMessageToServer', {text: newMessage})
+});
+
+socket.on('messageToClients', (msg) => {
+    console.log(msg);
+    document.querySelector('#messages').innerHTML += `<li>${msg.text}</li>`;
+});
